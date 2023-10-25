cfbeacfc25b3e30d6ad0de26b0d6d4cc1c6126c7
MatteoGunetto <matteogunetto@tiscali.it>
2023-10-25 17:43:53 +0200

    fix minori

--- src/org/lesson/java/eletronics/Main.java
+++ src/org/lesson/java/eletronics/Main.java
@@ -3,12 +3,12 @@ package org.lesson.java.eletronics;
 public class Main {
     public static void main(String[] args) {
 
-        Dispositivo play2 = Creator.create("Playstation");
-        play2.play();
-        play2.stop();
+        Dispositivo playPlayStation = Creator.create("Playstation");
+        playPlayStation.play();
+        playPlayStation.stop();
 
-        Dispositivo dvd = Creator.create("LettoreDVD");
-        dvd.play();
-        dvd.stop();
+        Dispositivo playDVD = Creator.create("LettoreDVD");
+        playDVD.play();
+        playDVD.stop();
     }
 }
